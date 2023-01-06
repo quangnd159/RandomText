392b3ce3bc0fba0d54ebd64dd754915230dfb3e1
Quang Nguyen <quangnd159@gmail.com>
2023-01-06 00:00:33 +0000

    playing around

--- replit.nix
+++ replit.nix
@@ -1,5 +1,7 @@
 { pkgs }: {
   deps = [
+    pkgs.python38Packages.pip
+    pkgs.python38Full
     pkgs.nodePackages.vscode-langservers-extracted
     pkgs.nodePackages.typescript-language-server
   ];
